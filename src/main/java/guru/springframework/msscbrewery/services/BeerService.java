fe8f0987e492aecb85830fca95a6ba05f79eedbf
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-12-28 19:09:04 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by Luca Moro on 28/12/2020 19:00.
+ */
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,23 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Created by Luca Moro on 28/12/2020 18:51.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
